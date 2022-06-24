0a043fb726b036f7fa58c8970df0b4dac28bc0ea
v-kirilov <v_kirilov87@protonmail.ch>
2022-06-24 21:55:14 +0300

    Problem 02

---
+++ CoursePreparation/P02 Forest Road/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace P02_Forest_Road
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cols = int.Parse(Console.ReadLine());
+            int rows = 2 * cols - 1;
+            
+            string[,] road = new string[rows, cols];
+            
+            for (int row = 0; row < road.GetLength(0); row++)
+            {
+                for (int col = 0; col < road.GetLength(1); col++)
+                {
+                    if (row == col)
+                    {
+                        road[row, col] = "*";
+                    }
+                    else if (row >= road.GetLength(1))
+                    {
+                        road[row, col] = ".";
+                        road[row,2*(road.GetLength(1)-1)-row] = "*";
+                    }
+                    else
+                    {
+                        road[row, col] = ".";
+                    }
+                }
+                for (int i = 0; i < road.GetLength(1); i++)
+                {
+                    Console.Write(road[row, i]);
+                }
+                Console.WriteLine();
+
+            }
+            
+        }
+    }
+}
