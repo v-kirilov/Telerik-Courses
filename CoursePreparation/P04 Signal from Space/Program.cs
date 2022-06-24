2101d66db951a2ec3de242849a83bfe911a8bc62
v-kirilov <v_kirilov87@protonmail.ch>
2022-06-25 00:07:50 +0300

    Problem 04

---
+++ CoursePreparation/P04 Signal from Space/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace P04_Signal_from_Space
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            StringBuilder str= new StringBuilder();
+            str.Append(input);
+            for (int i = 0; i < str.Length - 1; i++)
+            {
+                string repeat = str[i].ToString();
+
+                string nextRepeat = str[i+1].ToString();
+                if (repeat == nextRepeat)
+                {
+                    str = str.Remove(i, 1);
+                    i--;
+                }
+
+            }
+            Console.WriteLine(str);
+        }
+    }
+}
