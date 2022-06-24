4d5c1e69174b4035c39403f8134897af0ea64bc4
v-kirilov <v_kirilov87@protonmail.ch>
2022-06-25 00:08:29 +0300

    Problem 06

---
+++ CoursePreparation/P06 Array Search/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P06_Array_Search
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArr = Console.ReadLine()
+                .Split(',')
+                .Select(int.Parse)
+                .ToArray();
+            List<int> output = new List<int>();
+
+            for (int i = 1; i <= inputArr.Length; i++)
+            {
+                if (!inputArr.Contains(i))
+                {
+                    output.Add(i);
+                }
+            }
+            output.Sort();
+            Console.WriteLine(String.Join(",",output));
+        }
+
+     
+    }
+}
