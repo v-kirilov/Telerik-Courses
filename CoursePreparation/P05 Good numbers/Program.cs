8f2705244215c0224ec6bcca21e40d14e812adf0
v-kirilov <v_kirilov87@protonmail.ch>
2022-06-25 00:08:09 +0300

    Problem 05

---
+++ CoursePreparation/P05 Good numbers/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace P05_Good_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            int[] input = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            int start = input[0];
+            int end = input[1];
+            for (int i = start; i <= end; i++)
+            {
+                if (i <= 10)
+                {
+                    counter++;
+                    continue;
+                }
+                else
+                {
+                    bool isGood = true;
+                    int number = i;
+                    while (number != 0)
+                    {
+                        int divider = number % 10;
+                        if (divider == 0)
+                        {
+                            number = number / 10;
+                            continue;
+                        }
+                        if (i % divider != 0)
+                        {
+                            isGood = false;
+                            break;
+                        }
+                        number = number / 10;
+                    }
+                    if (isGood)
+                    {
+                        counter++;
+                    }
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
