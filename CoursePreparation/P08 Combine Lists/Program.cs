9ccc82cd4f9e61bda2d21a4868c65cb76cd6f4e4
v-kirilov <v_kirilov87@protonmail.ch>
2022-06-26 17:45:25 +0300

    Problem 08

---
+++ CoursePreparation/P08 Combine Lists/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P08_Combine_Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listOne = Console.ReadLine().Split(',').Select(int.Parse).ToList();
+            List<int> listTwo = Console.ReadLine().Split(',').Select(int.Parse).ToList();
+            List<int> merged = new List<int>();
+            for(int i = 0; i < listOne.Count; i++)
+            {
+                merged.Add(listOne[i]);
+                merged.Add(listTwo[i]);
+            }
+            Console.WriteLine(String.Join(",",merged));
+        }
+    }
+}
