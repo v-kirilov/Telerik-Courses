5e04921718ebb7226724818aaa0a45454ff95ece
v-kirilov <v_kirilov87@protonmail.ch>
2022-06-26 17:45:55 +0300

    Problem 10

---
+++ CoursePreparation/P10 Check for Play Card/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace P10_Check_for_Play_Card
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = new string[] { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+            string input = Console.ReadLine();
+            if (arr.Contains(input))
+            {
+                Console.WriteLine($"yes {input}");
+            }else
+            {
+                Console.WriteLine($"no {input}");
+
+            }
+
+        }
+    }
+}
