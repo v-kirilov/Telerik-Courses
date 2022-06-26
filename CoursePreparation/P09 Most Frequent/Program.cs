97fe1af170884587ff674b9ddf424a13eb41f13e
v-kirilov <v_kirilov87@protonmail.ch>
2022-06-26 17:45:41 +0300

    Problem 09

---
+++ CoursePreparation/P09 Most Frequent/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P09_Most_Frequent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            int topNumber = 0;
+            int maxCount=0;
+            for (int i = 0; i < count; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (dict.ContainsKey(number))
+                {
+                    dict[number]++;
+                }
+                else
+                {
+                dict[number] = 1;
+
+                }
+            }
+            foreach (var num in dict)
+            {
+                if (num.Value>maxCount)
+                {
+                    topNumber = num.Key;
+                    maxCount = num.Value;
+                }
+            }
+
+            Console.WriteLine($"{topNumber} ({maxCount} times)");
+        }
+    }
+}
