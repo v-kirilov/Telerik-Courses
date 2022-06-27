c49358999b6ecec86b5800d38c2b3fbb6d3c572d
v-kirilov <v_kirilov87@protonmail.ch>
2022-06-27 22:55:39 +0300

    Problem 01

---
+++ C# Core/P01 Reverse array/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace P01_Reverse_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            arr = arr.Reverse().ToArray();
+            Console.WriteLine(String.Join(", ",arr));
+        }
+    }
+}
