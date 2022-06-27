bfcafbf20f277b5a53517351e9f0a12500f719b7
v-kirilov <v_kirilov87@protonmail.ch>
2022-06-27 22:56:32 +0300

    Problem 05

---
+++ C# Core/P05 Three Groups/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace P05_Three_Groups
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i]%3==0)
+                {
+                    Console.Write($"{arr[i]} ");
+                }
+            }
+            Console.WriteLine();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 3 == 1)
+                {
+                    Console.Write($"{arr[i]} ");
+                }
+            }
+            Console.WriteLine();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 3 == 2)
+                {
+                    Console.Write($"{arr[i]} ");
+                }
+            }
+        }
+    }
+}
