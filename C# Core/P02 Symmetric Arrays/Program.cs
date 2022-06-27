184a121886fa57abe8fa448ec246e394cf7e180c
v-kirilov <v_kirilov87@protonmail.ch>
2022-06-27 22:55:52 +0300

    Problem 02

---
+++ C# Core/P02 Symmetric Arrays/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace P02_Symmetric_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            for (int j = 0; j < num; j++)
+            {
+                int[] arr = Console.ReadLine()
+               .Split(' ')
+               .Select(int.Parse)
+               .ToArray();
+                bool isMirrored = true;
+                for (int i = 0; i < arr.Length / 2; i++)
+                {
+                   
+                    if (arr[i] != arr[arr.Length - i - 1])
+                    {
+                        isMirrored = false;
+                        break;
+                    }
+                }
+                if (isMirrored)
+                {
+                    Console.WriteLine("Yes");
+                }
+                else
+                {
+                    Console.WriteLine("No");
+                }
+            }
+           
+        }
+    }
+}
