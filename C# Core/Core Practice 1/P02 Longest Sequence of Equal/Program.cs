f49a7bbe7c4e2fa8fb7b3f24fefcc66f23de1056
v-kirilov <v_kirilov87@protonmail.ch>
2022-06-28 20:44:09 +0300

    Problem 02

---
+++ C# Core/Core Practice 1/P02 Longest Sequence of Equal/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace P02_Longest_Sequence_of_Equal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int maxCount = 1;
+            int count = 1;
+            int pastNumber = 0;
+            for (int i = 0; i < num; i++)
+            {
+                int newNumber = int.Parse(Console.ReadLine());
+                if (i == 0)
+                {
+                    pastNumber = newNumber;
+                    continue;
+
+                }
+                if (pastNumber == newNumber)
+                {
+                    count++;
+                    if (count > maxCount)
+                    {
+                        maxCount = count;
+                    }
+                }
+                else
+                {
+                    count = 1;
+                }
+                pastNumber = newNumber;
+            }
+
+            Console.WriteLine(maxCount);
+        }
+    }
+}
