9cf594beea4c8e1186d1ac311fd152596b97210b
v-kirilov <v_kirilov87@protonmail.ch>
2022-06-28 20:43:57 +0300

    Problem 01

---
+++ C# Core/Core Practice 1/P01 Primes to N/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P01_Primes_to_N
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            List<int> primeList = new List<int>();
+            for (int i = 1; i <= num; i++)
+            {
+                primeList.Add(i);
+            }
+
+            for (int i = 1; i < primeList.Count; i++)
+            {
+
+                int candidate = primeList[i];
+                int sqrt = (int)Math.Sqrt(primeList[i]);
+                for (int j = 2; j <= sqrt; j++)
+                {
+                    if (candidate % j == 0)
+                    {
+                        primeList.Remove(primeList[i]);
+                        i--;
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine(String.Join(" ", primeList));
+        }
+    }
+}
