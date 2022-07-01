f3d579b0e2bcbd637cb7a0fa9795ab7014485651
v-kirilov <v_kirilov87@protonmail.ch>
2022-07-01 23:42:10 +0300

    Problem 01

---
+++ C# Core/Core Practice 3/P01 Longest Increasing Sequence/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace P01_Longest_Increasing_Sequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int pastNumber = 0;
+            int counter = 1;
+            int maxCounter = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                if (i == 0)
+                {
+                    pastNumber = currentNum;
+                    continue;
+                }
+                if (currentNum > pastNumber)
+                {
+                    counter++;
+                    if (counter > maxCounter)
+                    {
+                        maxCounter = counter;
+                    }
+                }
+                else
+                {
+                    counter = 1;
+                }
+                pastNumber = currentNum;
+            }
+            
+
+            Console.WriteLine(maxCounter);
+        }
+    }
+}
