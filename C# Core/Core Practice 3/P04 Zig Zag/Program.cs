6606353ba6a20f996b53fb89ae81f8548428d3a3
v-kirilov <v_kirilov87@protonmail.ch>
2022-07-05 14:56:30 +0300

    Update to get all 100 points in the tests.

--- C# Core/Core Practice 3/P04 Zig Zag/Program.cs
+++ C# Core/Core Practice 3/P04 Zig Zag/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Numerics;
+using System.Diagnostics;
 
 namespace P04_Zig_Zag
 {
@@ -11,86 +13,58 @@ namespace P04_Zig_Zag
             int n = int.Parse(input[0]);
             int m = int.Parse(input[1]);
 
-            long[,] matrix = new long[n, m];
+            //Stopwatch stopWatch = new Stopwatch();
+            //stopWatch.Start();
 
+
+            //int[,] matrix = new int[n, m];
+            long sum = 0;
+            long number = 0;
+
+            //matrix[0, 0] = 1;
             for (int i = 0; i < n; i++)
             {
                 for (int j = 0; j < m; j++)
                 {
-                    if (i == 0 && j == 0)
+                    number = 0;
+                    //matrix[i, j] = matrix[i, j - 1] + 3;
+
+                    if (i % 2 == 0)
                     {
-                        matrix[i, j] = 1;
+                        if (j % 2 == 0)
+                        {
+                            number = (i + j) * 3 + 1;
+                            if (i > 0 && i < n-1 && j > 0 && j < m-1)
+                            {
+                                sum += number * 2;
+                            }
+                            else
+                            {
+                                sum += number;
+                            }
+                        }
                     }
-                    else if (j == 0)
+                    else if (i % 2 != 0)
                     {
-                        matrix[i, j] = matrix[i - 1, j] + 3;
-                    }
-                    else
-                    {
-                        matrix[i, j] = matrix[i, j - 1] + 3;
+                        if (j % 2 != 0)
+                        {
+                            number = (i + j) * 3 + 1;
+                            if (i > 0 && i < n-1 && j > 0 && j < m-1)
+                            {
+                                sum += number * 2;
+
+                            }
+                            else
+                            {
+                                sum += number;
+                            }
+                        }
+
                     }
                 }
             }
-            int currentRow = 0;
-            int currentCol = 0;
-            long sum = 1;
 
-            int nextRow = currentRow;
-            int nextCol = currentCol;
 
-            for (int i = 0; i < matrix.GetLength(1) / 2; i++)
-            {
-                while (true)
-                {
-                    if (nextRow + 1 < matrix.GetLength(0) && nextCol + 1 < matrix.GetLength(1))
-                    {
-                        nextRow++;
-                        nextCol++;
-                        sum += matrix[nextRow, nextCol];
-                    }
-                    else
-                    {
-                        break;
-                    }
-                    if (nextCol + 1 < matrix.GetLength(1))
-                    {
-                        nextCol++;
-                        nextRow--;
-                        sum += matrix[nextRow, nextCol];
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
-                while (true)
-                {
-                    if (nextRow + 1 < matrix.GetLength(0) && nextCol - 1 >= 0)
-                    {
-                        nextRow++;
-                        nextCol--;
-                        sum += matrix[nextRow, nextCol];
-                    }
-                    else
-                    {
-                        break;
-                    }
-                    if (nextCol - 1 >= 0)
-                    {
-                        nextRow--;
-                        nextCol--;
-                        sum += matrix[nextRow, nextCol];
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
-                if (nextCol == 0 && nextRow == matrix.GetLength(0) - 1)
-                {
-                    break;
-                }
-            }
             Console.WriteLine(sum);
         }
     }
